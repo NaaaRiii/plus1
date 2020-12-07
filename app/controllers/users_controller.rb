41f26e4052917979ce9596b21ef3eb8a4661923a
NaaaRiii <axenarikilo48698062@gmail.com>
2020-12-07 18:39:12 +0900

    マイページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  def edit
+  def show
     
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'enjoyments/index'
   root to:"enjoyments#index"
-  resources :users, only: [:index, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
 end
